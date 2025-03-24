965fcbd1525d066b2db800264a39cdfa2e360b5e
Christian Alfoni <christianalfoni@gmail.com>
2025-03-24 17:12:39 +0100

    expose type on namespace

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import { createProxy } from "./proxy";
 
 export { observer, Observer } from "./observer";
 
+export namespace reactive {
+  export type Data<T extends { id: string }> = {
+    data: Record<string, T>;
+    list: readonly T[];
+  };
+}
+
 export function reactive<T extends Record<string, any>>(value: T): T {
   return createProxy(value);
 }
