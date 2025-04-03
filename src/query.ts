1202d3d8c8e0d3349a9d445c7009268ba6960f14
Christian Alfoni <christianalfoni@gmail.com>
2025-04-03 08:30:43 +0200

    wip query

--- src/query.ts
+++ src/query.ts
@@ -1,21 +1,23 @@
 import { transaction } from "mobx";
 import { reactive } from ".";
 
-type IdleInternalState = {
+type IdleInternalState<T> = {
   current: "IDLE";
+  promise: Promise<T>;
+  resolve: (value: T) => void;
+  reject: (error: Error) => void;
 };
 
 type ActiveInternalState<T> = {
   current: "ACTIVE";
   abortController: AbortController;
   promise: Promise<T>;
-  status: Status<T>;
+  status: State<T>;
 };
 
-type InternalState<T> = ActiveInternalState<T> | IdleInternalState;
+type InternalState<T> = ActiveInternalState<T> | IdleInternalState<T>;
 
-type Status<T> = {
-  promise: Promise<T>;
+type State<T> = {
   isRevalidating: boolean;
 } & (
   | {
@@ -32,36 +34,51 @@ type Status<T> = {
 );
 
 type Query<T> = {
-  read: () => Status<T>;
-  revalidate: () => Status<T>;
+  state: State<T>;
+  promise: Promise<T>;
+  revalidate: () => Promise<T>;
+  fetch: () => Promise<T>;
 };
 
 export function query<T>(fetcher: () => Promise<T>) {
+  const idlePromise = createBlockingPromise<T>();
+
   let internalState: InternalState<T> = {
     current: "IDLE",
+    promise: idlePromise.promise,
+    resolve: idlePromise.resolve,
+    reject: idlePromise.reject,
   };
 
+  const state = reactive<State<T>>({
+    isRevalidating: false,
+    status: "PENDING",
+  });
+
   const queryState = reactive<Query<T>>({
-    read,
+    state,
+    get promise() {
+      return internalState.promise;
+    },
     revalidate,
+    fetch: executeQuery,
   });
 
   return queryState;
 
-  function updateStatus(prev: Status<T>, update: Status<T>): Status<T> {
-    // Just for typing
-    const anyPrev: any = prev;
-
+  function updateStatus(prev: State<T>, next: State<T>): State<T> {
     transaction(() => {
-      delete anyPrev.error;
-      delete anyPrev.value;
-      Object.assign(anyPrev, update);
+      // @ts-expect-error
+      delete prev.error;
+      // @ts-expect-error
+      delete prev.value;
+      Object.assign(prev, next);
     });
 
-    return anyPrev;
+    return prev;
   }
 
-  function fetch(): ActiveInternalState<T> {
+  function executeQuery(): ActiveInternalState<T> {
     if (internalState.current === "ACTIVE") {
       internalState.abortController.abort();
     }
@@ -109,7 +126,7 @@ export function query<T>(fetcher: () => Promise<T>) {
       promise,
       status:
         internalState.current === "IDLE"
-          ? reactive<Status<T>>({
+          ? reactive<State<T>>({
               status: "PENDING",
               promise,
               isRevalidating: true,
@@ -133,23 +150,32 @@ export function query<T>(fetcher: () => Promise<T>) {
     return newInternalState;
   }
 
-  function read(): Status<T> {
-    if (internalState.current === "ACTIVE") {
-      return internalState.status;
-    }
+  function fetch(): Promise<T> {
+    internalState = executeQuery();
 
-    internalState = fetch();
-
-    return internalState.status;
+    return internalState.promise;
   }
 
-  function revalidate(): Status<T> {
+  function revalidate(): Promise<T> {
     if (internalState.current === "IDLE") {
-      return read();
+      return fetch();
     }
 
-    internalState = fetch();
+    internalState = executeQuery();
 
-    return internalState.status;
+    return internalState.promise;
   }
+}
+
+function createBlockingPromise<T>() {
+  let resolve: (value: T) => void;
+  let reject: (error: Error) => void;
+
+  const promise = new Promise<T>((r, r2) => {
+    resolve = r;
+    reject = r2;
+  });
+
+  // @ts-expect-error
+  return { promise, resolve, reject };
 }
